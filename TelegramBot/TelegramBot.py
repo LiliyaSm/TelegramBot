1e5dfc338809c58765a7754844165d90133e19b4
Liliia Smelova <liliia.smelova@outlook.com>
2019-11-22 16:56:08 +0300

    write parrot, sms functions

--- TelegramBot/TelegramBot.py
+++ TelegramBot/TelegramBot.py
@@ -1,13 +1,39 @@
 from telegram.ext import Updater
+from settings import TG_TOKEN, TG_API_URL
+from telegram.ext import CommandHandler
+from telegram.ext import MessageHandler
+from telegram.ext import Filters
+import json
 
-TOKEN = None
+def sms(bot, update):
+    print("'/start' was sent!")
+    bot.message.reply_text("Hello, {}" .format(bot.message.chat.first_name))
+    
+    #pprint.pprint(bot.message)
+    print(bot.message)
+    #print(json.dumps(bot.message, indent=2))
 
-with open("key.txt") as f:
-    TOKEN = f.read().strip()
+
+def parrot(bot, update):
+    print(bot.message.text)
+    bot.message.reply_text(bot.message.text)
 
 def main():
-    my_bot = Updater("TOKEN", "https://telegg.ru/otig/bot", use_context=True)
-    my_bot.start_polling()
-    my_bot.idle()
+    my_bot = Updater(TG_TOKEN, TG_API_URL, use_context=True)
+
+    my_bot.dispatcher.add_handler(CommandHandler('start',sms))
+    my_bot.dispatcher.add_handler(MessageHandler(Filters.text, parrot))
+
+
+    my_bot.start_polling() #проверяет наличие сбщ с платформы тлг
+    my_bot.idle() # бот работает пока его не остановят
+
+    
+
+
+
+
+
+
 
 main()
