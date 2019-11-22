36357b31151e77c249aef0f1466b6d6c1d03ae7b
Liliia Smelova <liliia.smelova@outlook.com>
2019-11-22 13:52:11 +0300

    token was added

--- TelegramBot/TelegramBot.py
+++ TelegramBot/TelegramBot.py
@@ -1,1 +1,13 @@
+from telegram.ext import Updater
 
+TOKEN = None
+
+with open("key.txt") as f:
+    TOKEN = f.read().strip()
+
+def main():
+    my_bot = Updater("TOKEN", "https://telegg.ru/otig/bot", use_context=True)
+    my_bot.start_polling()
+    my_bot.idle()
+
+main()
